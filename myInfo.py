097f3be86912be239d8a2a449cdb3788bfd0a5e1
Thomasschulz1 <91210712+Thomasschulz1@users.noreply.github.com>
2022-01-25 10:22:41 -0500

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175L-50
+#Thomas Schulz
+#myInfo
+print("My name is Thomas Schulz")
+print("My aderess is 43 Parkview Dr,Succasunna, 07876")
+print("My telephone number is 973-934-2251")
+print("My college major is computer science")
