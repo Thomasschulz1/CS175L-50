1d9bf41791c69ec8598515f88799dc0ae67b9d4b
Thomasschulz1 <91210712+Thomasschulz1@users.noreply.github.com>
2022-01-26 09:17:06 -0500

    updated file

--- myInfo.py
+++ myInfo.py
@@ -2,6 +2,6 @@
 #Thomas Schulz
 #myInfo
 print("My name is Thomas Schulz")
-print("My aderess is 43 Parkview Dr,Succasunna, 07876")
+print("My aderess is 43 Parkview Dr,Succasunna,New Jersey,07876")
 print("My telephone number is 973-934-2251")
 print("My college major is computer science")
